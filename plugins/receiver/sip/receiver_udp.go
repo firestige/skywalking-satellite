0844f9246c38f0a27d6443dfc8bebcf0e4f9ac2e
firestige <firestige@icloud.com>
2025-07-30 06:37:25 +0000

    fix(sip): add fallback for UDP truncation/parse failures
    When UDP message is truncated or fails to parse, implement fallback
    by directly passing raw UDP payload to gosip for parsing instead of
    dropping the packet.
    
    This serves as a temporary measure until proper SIP message validation
    and error handling can be implemented.

--- plugins/receiver/sip/receiver_udp.go
+++ plugins/receiver/sip/receiver_udp.go
@@ -1,21 +1,28 @@
 package sip
 
 import (
+	"encoding/binary"
+
 	"github.com/apache/skywalking-satellite/internal/pkg/log"
 	sip "github.com/apache/skywalking-satellite/plugins/receiver/sip/types"
 	packet "github.com/apache/skywalking-satellite/plugins/server/local/packet/types"
+	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
+	"github.com/sirupsen/logrus"
 )
 
 func (r *Receiver) processUDPFrame(frame *packet.RawFrameData) error {
 	// 解析SIP消息
+	// 由于可能存在gopacket不能正确识别SIP layer的情况，当gopacket无法解析时，直接使用GoSip解析UDP数据包
 	p := frame.Packet
-	sipLayer := p.Layer(layers.LayerTypeSIP)
-	if sipLayer == nil {
-		log.Logger.Debug("No SIP layer found in packet, ignore")
+	data := r.extraSIPByGoPacket(p)
+	if len(data) == 0 {
+		data = r.getUdpPayload(p)
+	}
+	if len(data) == 0 {
+		log.Logger.Debugf("No SIP data found in packet: %s", p)
 		return nil
 	}
-	data := sipLayer.LayerContents()
 	goSipMsg, err := r.sipParser.Parse(data)
 	if err != nil {
 		// TODO bad packet, ignore and continue, need statistics
@@ -52,4 +59,36 @@ func (r *Receiver) processUDPFrame(frame *packet.RawFrameData) error {
 
 	r.handler.HandleMessage(sipMsg)
 	return nil
+}
+
+func (r *Receiver) extraSIPByGoPacket(p gopacket.Packet) []byte {
+	sipLayer := p.Layer(layers.LayerTypeSIP)
+	if sipLayer == nil {
+		return nil
+	}
+	data := sipLayer.LayerContents()
+	if len(data) == 0 {
+		return nil
+	}
+	return data
+}
+
+func (r *Receiver) getUdpPayload(p gopacket.Packet) []byte {
+	udpLayer := p.Layer(layers.LayerTypeUDP)
+	if udpLayer == nil {
+		return nil
+	}
+	data := udpLayer.LayerContents()
+	if len(data) == 0 {
+		return nil
+	}
+	if log.Logger.IsLevelEnabled(logrus.DebugLevel) {
+		srcPort := binary.BigEndian.Uint16(data[0:2])
+		dstPort := binary.BigEndian.Uint16(data[2:4])
+		length := binary.BigEndian.Uint16(data[4:6])
+		checksum := binary.BigEndian.Uint16(data[6:8])
+		log.Logger.Debugf("UDP packet: srcPort=%d, dstPort=%d, length=%d, checksum=%d", srcPort, dstPort, length, checksum)
+		log.Logger.Debugf("UDP packet actual has: %d(inclue frame header)", len(data))
+	}
+	return data[8:] // Skip UDP header (8 bytes)
 }
